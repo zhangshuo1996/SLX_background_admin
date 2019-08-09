7be44e6f7496103a348359e22733e01fda6ef259
meng950813 <1752397568@qq.com>
2019-08-09 17:14:24 +0800

    by chen: 修复显示教师反馈数据的bug

--- web_admin/blueprints/teacher_info.py
+++ web_admin/blueprints/teacher_info.py
@@ -32,7 +32,7 @@ def get_info_by_tid():
     teacher_id = request.form.get('teacher_id', type=int)
     #根据_id从agent_feedback中获取商务提交的信息
     _id = ObjectId(request.form.get('_id'))
-    teacher_info_from_db = teacher_info_service.get_info_from_db(teacher_id,_id)
+    teacher_info_from_db = teacher_info_service.get_info_from_db(teacher_id, _id)
     return json.dumps(teacher_info_from_db)
 
 
@@ -46,72 +46,77 @@ def data_preservation():
     teacher_id = request.form.get('teacher_id')
     # 从前端得到的数据中得到的domain从字符串转化为list
     domain_str = request.form.get('domain')
-    if domain_str != ''and domain_str != None:
+    if domain_str != ''and domain_str is not None:
         domain = domain_str.split(' ')
     else:
         #防止domain出现['']的情况
         domain = []
     honor_str = request.form.get('honor_str')
     honor_title = []
-    if honor_str != '' and honor_str !=  None:
+    if honor_str != '' and honor_str is not None:
         honor_list = honor_str.split(',')
         for honor_dic_str in honor_list:
             honor_dic_list = honor_dic_str.split(' ')
-            honor_title.append({'type':honor_dic_list[1],'year':honor_dic_list[0]})
+            honor_title.append({'type': honor_dic_list[1], 'year': honor_dic_list[0]})
     obj_id = ObjectId(request.form.get('object_id'))
-    if teacher_id == "None" or teacher_id == None or teacher_id == '':
-    #处理新增数据
+    if teacher_id == "None" or teacher_id is None or teacher_id == '':
+        #处理新增数据
         data = {
             'name': request.form.get('name'),
-            'gender':request.form.get('gender'),
+            'gender': request.form.get('gender'),
             'school': request.form.get('school'),
             'institution': request.form.get('institution'),
             'department': request.form.get('department'),
             'birth_year': request.form.get('birth_year'),
             'title': request.form.get('title'),
-            'honor_title':honor_title,
+            'honor_title': honor_title,
             'domain': domain,
             'email': request.form.get('email'),
             'office_number': request.form.get('office_number'),
             'phone_number': request.form.get('phone_number'),
             'edu_exp': request.form.get('edu_exp'),
-            'work_exp':request.form.get('work_exp'),
+            'work_exp': request.form.get('work_exp'),
             'id': teacher_id,
             'position': request.form.get('position'),
             'patrnt_id': [],
             'funds_id': [],
             'paper_id': [],
-            'award_id':[]
+            'award_id': []
         }
 
         max_id = teacher_info_service.get_max_teacher_id()
         data['id'] = max_id+1
         try:
-            teacher_info_service.insert_basic_info(data,obj_id)
+            teacher_info_service.insert_basic_info(data, obj_id)
             return json.dumps({"success": True, "message": "操作成功"})
         except Exception as e:
             print(e)
             return json.dumps({"success": False, "message": "操作失败"})
     else:
-    #处理修改数据
-        data ={'name': request.form.get('name'),
-               'gender':request.form.get('gender'),
-                'school': request.form.get('school'),
-                'institution': request.form.get('institution'),
-               'department': request.form.get('department'),
-                'birth_year': request.form.get('birth_year'),
-                'title': request.form.get('title'),
-               'position':request.form.get('position'),
-               'honor_title': honor_title,
-               'domain': domain,
-                'email': request.form.get('email'),
-                'office_number': request.form.get('office_number'),
-                'phone_number': request.form.get('phone_number'),
-                'edu_exp': request.form.get('edu_exp'),
-               'work_exp':request.form.get('work_exp')
-               }
+        #处理修改数据
+        data = {
+            'name': request.form.get('name'),
+            'gender': request.form.get('gender'),
+            'school': request.form.get('school'),
+            'institution': request.form.get('institution'),
+            'department': request.form.get('department'),
+            'birth_year': request.form.get('birth_year'),
+            'title': request.form.get('title'),
+            'position': request.form.get('position'),
+            'honor_title': honor_title,
+            'domain': domain,
+            'email': request.form.get('email'),
+            'office_number': request.form.get('office_number'),
+            'phone_number': request.form.get('phone_number'),
+            'edu_exp': request.form.get('edu_exp'),
+            'work_exp': request.form.get('work_exp')
+        }
+        for k, value in data.items():
+            if value is None:
+                value = ""
+
         try:
-            teacher_info_service.update_basic_info(teacher_id,obj_id,data)
+            teacher_info_service.update_basic_info(teacher_id, obj_id, data)
             return json.dumps({"success": True, "message": "操作成功"})
         except Exception as e:
             print(e)
--- web_admin/service/teacher_info_service.py
+++ web_admin/service/teacher_info_service.py
@@ -18,7 +18,7 @@ def get_modify_info():
     agent_feedback = mogo_operator.get_collection('agent_feedback')
     # status=1表示未处理的信息
     modify_info = list(agent_feedback.find({'status': 1}))
-    #商务先提交的排在前面
+    # 商务先提交的排在前面
     modify_info.sort(key=lambda x: x['timestamp'])
     # 处理数据
     for info in modify_info:
@@ -32,7 +32,8 @@ def get_modify_info():
         data_pretreate(info)
     return modify_info
 
-def get_info_from_db(teacher_id,_id):
+
+def get_info_from_db(teacher_id, _id):
     """
     将处理中的教师的信息从basic_info表中取出，以便于和商务提交的修改信息做对比
     :return: 该教师再basic_info表中的基本信息
@@ -40,11 +41,13 @@ def get_info_from_db(teacher_id,_id):
     mogo_operator = MongoOperator(**MongoDB_CONFIG)
     collection = mogo_operator.get_collection('basic_info')
     teacher_info_from_basic = collection.find_one({"id": teacher_id},
-                                               {'_id': 0, 'funds_id': 0, 'patent_id': 0, 'paper_id': 0,'award_id':0,'research_plan_id':0})
+                                                  {'_id': 0, 'funds_id': 0, 'patent_id': 0, 'paper_id': 0,
+                                                   'project_id': 0, 'award_id': 0, 'research_plan_id': 0})
     agent_feedback = mogo_operator.get_collection('agent_feedback')
-    teacher_info_from_feedback = agent_feedback.find_one({"_id":_id},)
-    teacher_info_from_db = data_treate(teacher_info_from_basic,teacher_info_from_feedback)
+    teacher_info_from_feedback = agent_feedback.find_one({"_id": _id})
+    teacher_info_from_db = data_treate(teacher_info_from_basic, teacher_info_from_feedback)
     return teacher_info_from_db
+
 
 def get_max_teacher_id():
     """
@@ -53,9 +56,10 @@ def get_max_teacher_id():
     """
     mogo_operator = MongoOperator(**MongoDB_CONFIG)
     collection = mogo_operator.get_collection('basic_info')
-    max_id_cursor= collection.find({}).sort([('id', -1)]).limit(1)
-    max_id =list(max_id_cursor)[0].get('id')
+    max_id_cursor = collection.find({}).sort([('id', -1)]).limit(1)
+    max_id = list(max_id_cursor)[0].get('id')
     return max_id
+
 
 def update_status(obj_id):
     """
@@ -68,13 +72,14 @@ def update_status(obj_id):
     except  Exception as e:
         print(e.args)
 
-def insert_basic_info(data,obj_id):
+
+def insert_basic_info(data, obj_id):
     """
     将一条记录插入basic_info表中,并根据_id将agent_feed中的那条数据status设置为0
     """
     mogo_operator = MongoOperator(**MongoDB_CONFIG)
     collection = mogo_operator.get_collection('basic_info')
-    agent_feedback_collection= mogo_operator.get_collection('agent_feedback')
+    agent_feedback_collection = mogo_operator.get_collection('agent_feedback')
     try:
         collection.insert_one(data)
         # 将商务反馈的记录标注为已处理
@@ -82,7 +87,8 @@ def insert_basic_info(data,obj_id):
     except Exception as e:
         print(e.args)
 
-def update_basic_info(teacher_id,obj_id,data):
+
+def update_basic_info(teacher_id, obj_id, data):
     """
     修改basic_info中的一条记录，并根据_id将agent_feed中的那条数据status设置为0
     :return:
@@ -90,27 +96,28 @@ def update_basic_info(teacher_id,obj_id,data):
     mogo_operator = MongoOperator(**MongoDB_CONFIG)
     collection = mogo_operator.get_collection('basic_info')
     try:
-        agent_feedback_collection= mogo_operator.get_collection('agent_feedback')
+        agent_feedback_collection = mogo_operator.get_collection('agent_feedback')
         collection.update_one({'id': int(teacher_id)}, {'$set': {'name': data.get('name'),
-                                                                 'gender':data.get('gender'),
-                                                                      'school': data.get('school'),
-                                                                      'institution': data.get('institution'),
-                                                                        'department':data.get('department'),
-                                                                      'birth_year': data.get('birth_year'),
-                                                                      'title': data.get('title'),
-                                                                      'position':data.get('position'),
-                                                                      'domain': data.get('domain'),
-                                                                      'honor_title':data.get('honor_title'),
-                                                                      'email': data.get('email'),
-                                                                      'office_number': data.get('office_number'),
-                                                                      'phone_number': data.get('phone_number'),
-                                                                      'edu_exp': data.get('edu_exp'),
-                                                                        'work_exp':data.get('work_exp')
-                                                                        }})
+                                                                 'gender': data.get('gender'),
+                                                                 'school': data.get('school'),
+                                                                 'institution': data.get('institution'),
+                                                                 'department': data.get('department'),
+                                                                 'birth_year': data.get('birth_year'),
+                                                                 'title': data.get('title'),
+                                                                 'position': data.get('position'),
+                                                                 'domain': data.get('domain'),
+                                                                 'honor_title': data.get('honor_title'),
+                                                                 'email': data.get('email'),
+                                                                 'office_number': data.get('office_number'),
+                                                                 'phone_number': data.get('phone_number'),
+                                                                 'edu_exp': data.get('edu_exp'),
+                                                                 'work_exp': data.get('work_exp')
+                                                                 }})
         # 将商务反馈的记录标注为已处理
         agent_feedback_collection.update_one({'_id': obj_id}, {'$set': {'status': 0}})
     except Exception as e:
         print(e.args)
+
 
 def get_school():
     """
@@ -131,7 +138,7 @@ def get_institution(school_name):
     """
     mongo_operator = MongoOperator(**MongoDB_CONFIG)
     school = mongo_operator.get_collection("basic_info")
-    institution = school.distinct("institution",{"school":school_name})
+    institution = school.distinct("institution", {"school": school_name})
     return institution
 
 
@@ -145,12 +152,13 @@ def get_teacher(school, institution):
     """
     mongo = MongoOperator(**MongoDB_CONFIG)
     basic_info_col = mongo.get_collection("basic_info")
-    teacher_list = list(basic_info_col.find({"school": school, "institution": institution}, {"_id": 0, "name": 1, "id": 1, "department": 1}))
+    teacher_list = list(basic_info_col.find({"school": school, "institution": institution},
+                                            {"_id": 0, "name": 1, "id": 1, "department": 1}))
 
     return teacher_list
 
 
-def get_teacher_info(school,institution,teacher):
+def get_teacher_info(school, institution, teacher):
     """
     根据学校名，学院名和教师名获取教师信息
     :param school:学校名
@@ -161,9 +169,9 @@ def get_teacher_info(school,institution,teacher):
     mongo_operator = MongoOperator(**MongoDB_CONFIG)
     teacher_list = mongo_operator.get_collection("basic_info")
     if institution == "":
-        teacher_dict = teacher_list.find_one({"school":school,"institution":institution,"name":teacher})
+        teacher_dict = teacher_list.find_one({"school": school, "institution": institution, "name": teacher})
     else:
-        teacher_dict = teacher_list.find_one({"school":school,"name":teacher})
+        teacher_dict = teacher_list.find_one({"school": school, "name": teacher})
     return teacher_dict
 
 
@@ -185,7 +193,7 @@ def update_dept(dept_info):
         basic_info_col.update({"id": int(dict["teacher_id"])}, {"$set": {"department": dict["department"].strip()}})
 
 
-def update_teacher(id,teacher_info):
+def update_teacher(id, teacher_info):
     """
     根据教师id和教师消息更新教师的消息
     :param id:教师id
@@ -194,7 +202,8 @@ def update_teacher(id,teacher_info):
     """
     mongo_operator = MongoOperator(**MongoDB_CONFIG)
     teacher_list = mongo_operator.get_collection("basic_info")
-    teacher_list.update_one({"id":id},{"$set":teacher_info})
+    teacher_list.update_one({"id": id}, {"$set": teacher_info})
+
 
 def delete_teacher(id):
     """
@@ -204,7 +213,8 @@ def delete_teacher(id):
     """
     mongo_operator = MongoOperator(**MongoDB_CONFIG)
     teacher_list = mongo_operator.get_collection("basic_info")
-    teacher_list.update_one({"id":id},{"$set":{"status":0}})
+    teacher_list.update_one({"id": id}, {"$set": {"status": 0}})
+
 
 def add_teacher(teacher_info):
     """
@@ -218,6 +228,7 @@ def add_teacher(teacher_info):
     teacher_info['id'] = max_id
     teacher_list.insert_one(teacher_info)
 
+
 def data_pretreate(info):
     """
     预先处理从数据库取出的信息，由于多个函数用到，所以定义一个函数，以便调用和修改
@@ -227,7 +238,7 @@ def data_pretreate(info):
         info['timestamp'] = str(info['timestamp'])
     if '_id' in info:
         info['_id'] = str(info['_id'])
-    #将domain和honor转为字符串，便于前端展示
+    # 将domain和honor转为字符串，便于前端展示
     if 'domain' in info:
         if info['domain'] != [] and info['domain'] != None:
             domain_str = ''
@@ -247,21 +258,21 @@ def data_pretreate(info):
         else:
             for honor_title_dic in info['honor_title']:
                 if honor_title_str != '':
-                    #year和type之间是一个空格，而两个头衔之间是一个逗号
-                    honor_title_str += (','+str(honor_title_dic['year'])+' '+honor_title_dic['type'])
+                    # year和type之间是一个空格，而两个头衔之间是一个逗号
+                    honor_title_str += (',' + str(honor_title_dic['year']) + ' ' + honor_title_dic['type'])
                 else:
-                    honor_title_str += (str(honor_title_dic['year'])+' '+honor_title_dic['type'])
+                    honor_title_str += (str(honor_title_dic['year']) + ' ' + honor_title_dic['type'])
             info['honor_title'] = honor_title_str
-    #防止页面出现undefined
+    # 防止页面出现undefined
     if 'department' in info:
         if info['department'] == None:
             info['department'] = ''
     else:
-        department_dic = {'department':''}
+        department_dic = {'department': ''}
         info.update(department_dic)
 
 
-def data_treate(teacher_info_from_basic,teacher_info_from_feedback):
+def data_treate(teacher_info_from_basic, teacher_info_from_feedback):
     """
     将agent_feedback中的数据和basic_info中的数据做对比，将teacher_info_from_db中有的字段而teacher_info_from_feedback中没有的字段添加到teacher_info_from_feeedback中
     ，以区分到底商务发来的数据中没有这个字段，还是这个字段为空。
@@ -271,47 +282,47 @@ def data_treate(teacher_info_from_basic,teacher_info_from_feedback):
         if key not in teacher_info_from_feedback:
             teacher_info_from_feedback[key] = teacher_info_from_basic[key]
 
-    #处理honor_title字段，basic_info里hornor_title是[{'type':'','year':''}]，而agent_feedback中是['荣誉称号,'’,...]
+    # 处理honor_title字段，basic_info里hornor_title是[{'type':'','year':''}]，而agent_feedback中是['荣誉称号,'’,...]
     if 'honor_title' in teacher_info_from_feedback:
-        #两个都是[] 或者feedback字典中的数据来自basic_info
+        # 两个都是[] 或者feedback字典中的数据来自basic_info
         if teacher_info_from_feedback['honor_title'] == teacher_info_from_basic['honor_title']:
             pass
-        #teacher_info_from_feedback中honor_title不为空，而teacher_info_basic中的为空,将其转为[{'type':'','year':''}]格式
+        # teacher_info_from_feedback中honor_title不为空，而teacher_info_basic中的为空,将其转为[{'type':'','year':''}]格式
         elif teacher_info_from_feedback['honor_title'] != [] and teacher_info_from_basic['honor_title'] == []:
             honor_title_list = []
             for title in teacher_info_from_feedback['honor_title']:
-                honor_title_list.append({'type':title,'year':''})
+                honor_title_list.append({'type': title, 'year': ''})
             teacher_info_from_feedback['honor_title'] = honor_title_list
-        #两者都不为空,将basic中的年份添加到feedback中
+        # 两者都不为空,将basic中的年份添加到feedback中
         else:
             honor_title_list = []
             for title in teacher_info_from_feedback['honor_title']:
-                #标志
+                # 标志
                 flag = 0
                 for honor_dic in teacher_info_from_basic['honor_title']:
                     if title == honor_dic.get('type'):
-                        flag =1
-                        honor_title_list.append({'type':title,'year':honor_dic.get('year')})
+                        flag = 1
+                        honor_title_list.append({'type': title, 'year': honor_dic.get('year')})
                 if flag == 0:
                     honor_title_list.append({'type': title, 'year': ''})
                 flag = 0
             teacher_info_from_feedback['honor_title'] = honor_title_list
 
-            #将从数据库中取出的数据处理一下，便于展示
+            # 将从数据库中取出的数据处理一下，便于展示
     data_pretreate(teacher_info_from_basic)
     data_pretreate(teacher_info_from_feedback)
     teacher_data = {
-        'teacher_info_from_feedback':teacher_info_from_feedback,
-        'teacher_info_from_basic':teacher_info_from_basic
+        'teacher_info_from_feedback': teacher_info_from_feedback,
+        'teacher_info_from_basic': teacher_info_from_basic
     }
-    return  teacher_data
-
-
+    return teacher_data
 
 
 if __name__ == "__main__":
-    #测试
-    teacher_info = {'name': '王冬梅', 'school': '中国农业大学', 'institution': '马克思主义学院', 'title': '教授', 'birth_year': '', 'email': '11@qq.com', 'office_number': ' 1234', 'phone_number': '111', 'edu_exp': '1994-1997 北京大学，获得硕士学位2004—2008 中国农业大学，获得博士学位'}
+    # 测试
+    teacher_info = {'name': '王冬梅', 'school': '中国农业大学', 'institution': '马克思主义学院', 'title': '教授', 'birth_year': '',
+                    'email': '11@qq.com', 'office_number': ' 1234', 'phone_number': '111',
+                    'edu_exp': '1994-1997 北京大学，获得硕士学位2004—2008 中国农业大学，获得博士学位'}
 
-    update_teacher(73927,teacher_info)
+    update_teacher(73927, teacher_info)
     # add_teacher(teacher_info)
